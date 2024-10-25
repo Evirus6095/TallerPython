abb3ce18fe9436182449d4283bd22d9f1601ca5f
Evirus <l22240015@smartin.tecnm.mx>
2024-10-25 15:33:03 -0600

    Primeros programa

---
+++ estructurada/2.Variables.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a=int(input("Dame un número"))
+    b=int(input("Dame otro número"))
+    c=int(input("Con este número terminamos"))
+
+    resultado=a*a+b*b+c*c
+    print(f"El resultado es {resultado}")
+    print(f"El resultado es ",resultado)
