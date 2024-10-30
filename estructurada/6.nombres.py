4f02fb324004e4c2ae0a63e163902eed28bfecec
Luis Enrique Domínguez Olivares <lenriquedolimenzares6095@gmail.com>
2024-10-30 13:59:58 -0600

    Ejercicios

---
+++ estructurada/6.nombres.py
@@ -0,0 +1,8 @@
+if __name__=='__main__':
+    nom = input("Proporciona un nombre: ")
+match nom:
+    case 'Monica':print(f"El 02/09/81 nacio: {nom}")
+    case 'Antonio':print(f"El 05/03/81 nacio: {nom}")
+    case 'Jose':print(f"El 11/05/2001 nacio: {nom}")
+    case 'Ingrid':print(f"El 21/09/2010  nacio: {nom}")
+    case 'Gonzalo':print(f"El 12/02/2001  nacio: {nom}")
