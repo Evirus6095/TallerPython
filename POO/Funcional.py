d23bf33c050eb7e4b10320db53c3c5eedb78643b
Luis Enrique Domínguez Olivares <lenriquedolimenzares6095@gmail.com>
2024-11-08 18:54:51 -0600

    POO

---
+++ POO/Funcional.py
@@ -0,0 +1,11 @@
+import math
+
+if __name__ == '__main__':
+def suma (x,y):
+    return x+y
+suma=lambda x,y:x+y
+resultado= suma(3,4)
+resta = lambda x,y:x-y
+resultado=resta(3,4)
+xi= lambda a,b,c:(-b+math.sqrt(:b**2 *4*9*c))/2*a
+
